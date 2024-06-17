4c5043eec14be98dfc20ade1efd22e2b66f96d89
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-06-17 16:43:30 +0200

    feat: add function to remove numbers

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Visualizzare in pagina 5 numeri casuali.
 // seleziono il contenitore
-let container = document.querySelector(".container");
+const container = document.querySelector(".container");
 
 // genero 5 numeri casuali da 1 a 100
 const numbers = generateRandomNumbersArray(1,100,5);
@@ -10,9 +10,13 @@ console.log(numbers);
 container.append(numbers);
 
 
-
-
 // Da lì parte un timer di 30 secondi.
-// Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+setTimeout(function(){
+    // Dopo 30 secondi i numeri scompaiono
+    container.remove(numbers);
+        
+}, 3000);
+
+
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
